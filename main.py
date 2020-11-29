c55da4e3ea3b67845a3f9380f817f654d53dfce5
James Brown <brownjames6571@gmail.com>
2020-11-29 11:05:55 +0000

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+import random
+
+redCards = ["rCard1", "rCard2", "rCard3", "rCard4", "rCard5", "rCard6", "rCard7", "rCard8", "rCard9", "rCard10"]
+
+yellowCards = ["yCard1", "yCard2", "yCard3", "yCard4", "yCard5", "yCard6", "yCard7", "yCard8", "yCard9", "yCard10"]
+
+blackCards = ["bCard1", "bCard2", "bCard3", "bCard4", "bCard5", "bCard6", "bCard7", "bCard8", "bCard9", "bCard10"]
+
+shuffle = "SHUFFLING CARDS..."
+
+p1 = 0
+p2 = 0
+
+p1name = input("Player 1 what is your name?")
+p2name = input("Player 2 what is your name?")
+
+print(shuffle)
+print(f"{p1name} take a card.")
